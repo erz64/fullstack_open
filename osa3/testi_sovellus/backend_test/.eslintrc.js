b36d5091cb7748edf9a7047424ad89143a5b4287
erz64 <eeroranta111@gmail.com>
2023-07-31 17:17:34 +0300

    some changes to the test application

---
+++ osa3/testi_sovellus/backend_test/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'google',
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true },
+    ],
+    'no-console': 0,
+  },
+}
+
